59522fd7ce7ced8588f33376e8a926b8f785ff0e
rodgersochieng <sirrodgers21@gmail.com>
2024-09-24 23:37:33 +0300

    makin ,cardata in json

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
+// import React from 'react';
+// import { FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
 
-const Navbar = () => {
-  return (
-    <nav className="bg-gradient-to-r from-blue-600 to-blue-800 p-4 flex justify-between items-center flex-wrap shadow-lg">
-      <div className="flex items-center space-x-2">
-        <span className="text-3xl">🚗</span> {/* Car emoji for logo */}
-        <span className="text-white text-2xl font-semibold">Car Dealership</span>
-      </div>
-      <div className="text-white flex items-center space-x-6">
-        <span className="flex items-center">
-          <FaEnvelope className="mr-1" />
-          info@cardealership.com
-        </span>
-        <span className="flex items-center">
-          <FaPhoneAlt className="mr-1" />
-          +254729942447
-        </span>
-      </div>
-    </nav>
-  );
-};
+// const Navbar = () => {
+//   return (
+//     <nav className="bg-gradient-to-r from-blue-600 to-blue-800 p-4 flex justify-between items-center flex-wrap shadow-lg">
+//       <div className="flex items-center space-x-2">
+//         <span className="text-3xl">🚗</span> {/* Car emoji for logo */}
+//         <span className="text-white text-2xl font-semibold">Car Dealership</span>
+//       </div>
+//       <div className="text-white flex items-center space-x-6">
+//         <span className="flex items-center">
+//           <FaEnvelope className="mr-1" />
+//           info@cardealership.com
+//         </span>
+//         <span className="flex items-center">
+//           <FaPhoneAlt className="mr-1" />
+//           +254729942447
+//         </span>
+//       </div>
+//     </nav>
+//   );
+// };
 
-export default Navbar;
+// export default Navbar;
